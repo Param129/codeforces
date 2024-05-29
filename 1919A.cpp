e25aaef278f03c41a048d610737ae4eaae62a11f
Param Tomar <paramtomar81@gmail.com>
2024-05-29 09:26:56 +0530

    day9 1questions

--- 1919A.cpp
+++ 1919A.cpp
@@ -8,9 +8,9 @@ int main(){
         int a,b;
         cin>>a>>b;
 
-        if((a%2==0 && b%2==0) || (a%2!=0 && b%2!=0))cout<<"BOB"<<endl;
+        if((a%2==0 && b%2==0) || (a%2!=0 && b%2!=0))cout<<"Bob"<<endl;
         else{
-            cout<<"ALICE"<<endl;
+            cout<<"Alice"<<endl;
         }
     }
 return 0;
