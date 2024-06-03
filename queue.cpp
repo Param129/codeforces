18e209e4678e6eba35b3decb47a40a5e920b2e29
Param Tomar <paramtomar81@gmail.com>
2024-06-03 11:43:09 +0530

    day14 3questions

--- queue.cpp
+++ queue.cpp
@@ -1,10 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int main(){
-    int n;
-    cin>>n;
-    vector<int>v(n);
-    
-return 0;
+int n,i,x[100000],y[100000],c;
+int main()
+{
+cin>>n;
+for (i=0;i<n;i++)
+{
+cin>>x[i];
+y[i]=x[i];
 }
+for (i=n-2;i>=0;i--)
+     y[i]=min(y[i+1],x[i]);
+
+for (i=0;i<n;i++)
+{
+	int* c=lower_bound(y+i+1,y+n,x[i]);
+	if (c-y-1==i)
+       cout<<-1<<" ";
+	else
+	   cout<<c-y-2-i<<" ";
+	
+}
+}
+ 
