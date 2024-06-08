7c416fa1377163e899aef081ced8af75ac67575a
Param Tomar <paramtomar81@gmail.com>
2024-06-08 10:16:24 +0530

    day19 3questions

---
+++ 1931C.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+
+        int left=1;
+    int right=1;
+    for(int i=1;i<n;i++){
+        if(a[i]!=a[i-1])break;
+        left++;
+    }
+    for(int i=n-2;i>=0;i--){
+        if(a[i]!=a[i+1])break;
+        right++;
+    }
+    int sum=0;
+    if(a[0]==a[n-1]){
+        sum=min(n,left+right);
+    }
+    else{
+        sum=min(n,max(left,right));
+    }
+    cout<<(n-sum)<<"\n";
+    }
+return 0;
+}
