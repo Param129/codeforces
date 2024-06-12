c5e145a4f68ff367aaf6236802754dd3a6e385a4
Param Tomar <paramtomar81@gmail.com>
2024-06-12 15:38:00 +0530

    day23 3questions

---
+++ 1676C.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long t;
+    cin >> t;
+    while(t--){
+        long long n, m;
+        cin >> n >> m;
+        vector<string> v(n);
+
+        for(long long i = 0; i < n; i++){
+            cin >> v[i];
+        }
+
+        long long ans = LLONG_MAX;
+        
+        for(long long i = 0; i < n; i++){
+            for(long long j = i+1; j < n; j++){
+                long long cost = 0;
+                for(long long k = 0; k < m; k++){
+                    cost += abs(v[i][k] - v[j][k]);
+                }
+                ans = min(ans, cost);
+            }
+        }
+
+        cout << ans << endl;
+    }
+    return 0;
+}
